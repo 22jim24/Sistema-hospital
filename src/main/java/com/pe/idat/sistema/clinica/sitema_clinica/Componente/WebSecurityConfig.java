690a4e713d878307de58595e2259fa17a3040fe2
22jim24 <jnarazas22.cat@gmail.com>
2026-08-10 00:01:21 -0500

    Update WebSecurityConfig.java

--- src/main/java/com/pe/idat/sistema/clinica/sitema_clinica/Componente/WebSecurityConfig.java
+++ src/main/java/com/pe/idat/sistema/clinica/sitema_clinica/Componente/WebSecurityConfig.java
@@ -36,8 +36,8 @@ public class WebSecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/especialidad/**").hasAuthority("ROLE_ACTIVO_PACIENTE")
                         .requestMatchers(HttpMethod.GET, "/turno/**").hasAuthority("ROLE_ACTIVO_PACIENTE")
                         .requestMatchers(HttpMethod.GET, "/paciente/**").hasAuthority("ROLE_ACTIVO_PACIENTE")
-                        .requestMatchers(HttpMethod.GET, "/medico/**").hasAuthority("ROLE_ACTIVO_PACIENTE") // <-- AGREGAR ESTA
-                        .requestMatchers(HttpMethod.GET, "/cita/paciente/**").hasAuthority("ROLE_ACTIVO_PACIENTE") // <-- AGREGAR ESTA
+                        .requestMatchers(HttpMethod.GET, "/medico/**").hasAuthority("ROLE_ACTIVO_PACIENTE") 
+                        .requestMatchers(HttpMethod.GET, "/cita/paciente/**").hasAuthority("ROLE_ACTIVO_PACIENTE") 
                         .requestMatchers(HttpMethod.POST, "/cita").hasAuthority("ROLE_ACTIVO_PACIENTE")
 
                         .requestMatchers("/historial/**").hasAuthority("ROLE_ACTIVO_MEDICO")
